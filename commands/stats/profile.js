28b1f5d963faec8e8e873c3f22ae17cd5f83f8f1
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2020-05-12 12:27:21 +0200

    Clean up code

--- commands/stats/profile.js
+++ commands/stats/profile.js
@@ -1,87 +1,105 @@
-const { Command } = require('discord.js-commando');
-const { RichEmbed } = require('discord.js');
-const request = require('request');
+const { Command } = require("discord.js-commando");
+const { RichEmbed } = require("discord.js");
+const request = require("request");
 
 module.exports = class profile extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'profile',
-			group: 'stats',
-			memberName: 'profile',
-			description: 'Gets players stats',
-      args: [
-	            {
-		              key: 'username',
-		              prompt: 'What is players username?',
-		              type: 'string',
-              },
-	            {
-		              key: 'keymode',
-		              prompt: 'What is this other useless thing?',
-		              type: 'string',
-                  default: '--4k',
-	             },
-          ]
-		});
-	}
-  run(message, { username,keymode }) {
-		var linkname = username.replace(/_/g, '%20');
-		var keysmode = 1
-		if (keymode == "--7k") {
-			var keysmode = 2
-		} else {
-			var keysmode = 1
-		}
-    request.get("https://api.quavergame.com/v1/users/full/" + linkname, {json:true}, (error, response, body) => {
-      if (!error && body.status == 200) {
-				var name = username.replace(/%20/g, ' ');
-				name = username.replace(/_/g, ' ');
-				let embed = new RichEmbed()
-		    embed.setColor(0x44e8ff)
-				if (keysmode == 1) {
-					embed.setTitle("Global: #" + body.user.keys4.globalRank)
-					embed.setAuthor("Stats for " + name + " (4K)")
-					if (body.user.info.avatar_url != 'undefined') {
-						embed.setThumbnail(body.user.info.avatar_url)
-					} else {
-						embed.setThumbnail('https://i.imgur.com/mYYW5EO.png')
-					}
-			    embed.setDescription("Country Ranking: #" + body.user.keys4.countryRank)
-			    embed.addField("Statistics",'Overall Rating: ' + Math.round(body.user.keys4.stats.overall_performance_rating * 100) / 100 + '\nRanked Score: ' + body.user.keys4.stats.ranked_score.toLocaleString() + '\nTotal Score: ' + body.user.keys4.stats.total_score.toLocaleString() + '\nAccuracy: ' + Math.round(body.user.keys4.stats.overall_accuracy * 100) / 100 + '\nPlay Count: ' + body.user.keys4.stats.play_count.toLocaleString() + '\nMax Combo: ' + body.user.keys4.stats.max_combo.toLocaleString())
-			    embed.setTimestamp()
-			    embed.setFooter('https://quavergame.com')
-					message.embed(embed)
-				} else if (keysmode == 2) {
-					embed.setTitle("Global: #" + body.user.keys7.globalRank)
-					embed.setAuthor("Stats for " + name + " (7K)")
-					if (body.user.info.avatar_url != 'undefined') {
-						embed.setThumbnail(body.user.info.avatar_url)
-					} else {
-						embed.setThumbnail('https://i.imgur.com/mYYW5EO.png')
-					}
-			    embed.setDescription("Country Ranking: #" + body.user.keys7.countryRank)
-			    embed.addField("Statistics",'Overall Rating: ' + Math.round(body.user.keys7.stats.overall_performance_rating * 100) / 100 + '\nRanked Score: ' + body.user.keys7.stats.ranked_score.toLocaleString() + '\nTotal Score: ' + body.user.keys7.stats.total_score.toLocaleString() + '\nAccuracy: ' + Math.round(body.user.keys7.stats.overall_accuracy * 100) / 100 + '\nPlay Count: ' + body.user.keys7.stats.play_count.toLocaleString() + '\nMax Combo: ' + body.user.keys7.stats.max_combo.toLocaleString())
-			    embed.setTimestamp()
-			    embed.setFooter('https://quavergame.com')
-					message.embed(embed)
-				}
-		    //.setURL(url)
-      } else if (!error && body.status == 404) {
-				var name = username.replace(/%20/g, ' ');
-				let embed = new RichEmbed()
-				embed.setColor(0x44e8ff)
-				embed.setAuthor("Stats for " + name)
-				embed.setThumbnail('https://i.imgur.com/mYYW5EO.png')
-				embed.setDescription("This user does not exist!")
-				embed.addField("Statistics","No data can be shown!")
-				embed.setTimestamp()
-				embed.setFooter('https://quavergame.com')
-				message.channel.send(embed)
-      } else {
-       console.error("Unable to send message.")
-       console.error(response)
-       console.error(error)
-      }
-    })
-	}
+    constructor(client) {
+        super(client, {
+            name: "profile",
+            group: "stats",
+            memberName: "profile",
+            description: "Gets players stats",
+            args: [
+                {
+                    key: "username",
+                    prompt: "What is players username?",
+                    type: "string",
+                },
+                {
+                    key: "keymode",
+                    prompt: "What is this other useless thing?",
+                    type: "string",
+                    default: "--4k",
+                },
+            ]
+        });
+    }
+    run(message, { username, keymode }) {
+        let keysmode = 1;
+        if (keymode == "--7k")
+            keysmode = 2
+
+        let url = "https://api.quavergame.com/v1/users/full/" + username.replace(/_/g, "%20");
+
+        request.get(url, { json: true }, (error, response, body) => {
+
+            if (!error && body.status == 200) {
+                let name = username.replace(/%20/g, " ").replace(/_/g, " ");
+                let embed = new RichEmbed();
+
+                if (body.user.info.avatar_url != "undefined")
+                    embed.setThumbnail(body.user.info.avatar_url);
+                else
+                    embed.setThumbnail("https://i.imgur.com/mYYW5EO.png");
+
+                let keymodeObject = null;
+                let keymodeString = "";
+
+                switch (keysmode) {
+                    case 1:
+                        keymodeObject = body.user.keys4;
+                        keymodeString = "4K";
+                        break;
+                    case 2:
+                        keymodeObject = body.user.keys7;
+                        keymodeString = "7K";
+                    default:
+                        break;
+                }
+
+                let stats = {
+                    "Overall Rating": Math.round(keymodeObject.stats.overall_performance_rating * 100) / 100,
+                    "Ranked Score": keymodeObject.stats.ranked_score,
+                    "Total Score": keymodeObject.stats.total_score,
+                    "Accuracy": Math.round(keymodeObject.stats.overall_accuracy * 100) / 100,
+                    "Play Count": keymodeObject.stats.play_count,
+                    "Max Combo": keymodeObject.stats.max_combo,
+
+                };
+
+                let statisticsString = "";
+                for (const key in stats)
+                    statisticsString += `${key}: ${stats[key].toLocaleString()}\n`;
+
+                embed.setColor(0x44e8ff);
+                embed.setTitle("Global: #" + keymodeObject.globalRank);
+                embed.setAuthor(`Stats for ${name} (${keymodeString})`);
+                embed.setDescription("Country Ranking: #" + keymodeObject.countryRank);
+                embed.addField("Statistics", statisticsString.trim());
+                embed.setTimestamp();
+                embed.setFooter("https://quavergame.com");
+                message.embed(embed); //.setURL(url)
+
+            } else if (!error && body.status == 404) {
+
+                let name = username.replace(/%20/g, " ");
+                let embed = new RichEmbed()
+                embed.setColor(0x44e8ff)
+                embed.setAuthor("Stats for " + name)
+                embed.setThumbnail("https://i.imgur.com/mYYW5EO.png")
+                embed.setDescription("This user does not exist!")
+                embed.addField("Statistics", "No data can be shown!")
+                embed.setTimestamp()
+                embed.setFooter("https://quavergame.com")
+                message.channel.send(embed)
+
+            } else {
+
+                console.error("Unable to send message.");
+                console.error(response);
+                console.error(error);
+
+            }
+        })
+    }
 };
