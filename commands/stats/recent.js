2b51952b5e3411c747a77dfdb2c78ec58d67b9eb
ItzFireable <fireableplayz@gmail.com>
2020-05-12 17:41:10 +0300

    early version of recent

--- commands/stats/recent.js
+++ commands/stats/recent.js
@@ -25,15 +25,89 @@ module.exports = class recent extends Command {
         });
     }
     run(message, { username, keymode }) {
+
+        let keysmode = keymode == "--7k" ? 2 : 1;
         let name = username.replace(/%20/g, " ");
-        let embed = new RichEmbed()
-        embed.setColor(0x44e8ff)
-        embed.setAuthor("Recent score for " + name)
-        embed.setThumbnail("https://i.imgur.com/mYYW5EO.png")
-        embed.setDescription("Not implemented yet. Please try again later!")
-        embed.addField("Statistics", "No data can be shown!")
-        embed.setTimestamp()
-        embed.setFooter("https://quavergame.com")
-        message.channel.send(embed)
+        let url = "https://api.quavergame.com/v1/users/full/" + name;
+
+        request.get(url, { json: true }, (error, response, body) => {
+
+            if (!error && body.status == 200) {
+                
+                let embed = new RichEmbed()
+
+                embed.setColor(0x44e8ff)
+                embed.setAuthor("Recent score for " + name)
+                embed.setThumbnail("https://i.imgur.com/mYYW5EO.png")
+
+                let id = body.user.info.id;
+                let latest = "https://api.quavergame.com/v1/users/scores/recent?id=" + id + "&mode=" + keysmode + "&limit=1"
+
+                request.get(url, { json: true }, (error, response, body) => {
+                    if (!error && body.status == 200) {
+
+                        let stats = {
+                            "Overall Rating": Math.round(keymodeObject.stats.overall_performance_rating * 100) / 100,
+                            "Ranked Score": keymodeObject.stats.ranked_score,
+                            "Total Score": keymodeObject.stats.total_score,
+                            "Accuracy": Math.round(keymodeObject.stats.overall_accuracy * 100) / 100,
+                            "Play Count": keymodeObject.stats.play_count,
+                            "Max Combo": keymodeObject.stats.max_combo
+                        };
+        
+                        let statisticsString = "";
+                        for (const key in stats)
+                            statisticsString += `${key}: ${stats[key].toLocaleString()}\n`;
+        
+                        //embed.addField("Statistics", statisticsString.trim());
+                        console.log(statisticsString.trim())
+
+                        embed.setDescription(" - ")
+                        embed.setTimestamp()
+                        embed.setFooter("https://quavergame.com")
+                        message.channel.send(embed)
+
+                    } else if (!error && body.status == 200) {
+
+                        let embed = new RichEmbed()
+                        embed.setColor(0x44e8ff)
+                        embed.setAuthor("Recent score for " + name)
+                        embed.setThumbnail("https://i.imgur.com/mYYW5EO.png")
+                        embed.setDescription("An error occurred getting the score.")
+                        embed.addField("Statistics", "No data can be shown!")
+                        embed.setTimestamp()
+                        embed.setFooter("https://quavergame.com")
+
+                        message.channel.send(embed)
+                    } else {
+
+                        console.error("Unable to send message.");
+                        console.error(response);
+                        console.error(error);
+
+                    }
+                })
+
+            } else if (!error && body.status == 200) {
+
+                let embed = new RichEmbed()
+                embed.setColor(0x44e8ff)
+                embed.setAuthor("Recent score for " + name)
+                embed.setThumbnail("https://i.imgur.com/mYYW5EO.png")
+                embed.setDescription("This user does not exist!")
+                embed.addField("Statistics", "No data can be shown!")
+                embed.setTimestamp()
+                embed.setFooter("https://quavergame.com")
+                message.channel.send(embed)
+
+            } else {
+
+                console.error("Unable to send message.");
+                console.error(response);
+                console.error(error);
+
+            }
+        })
+
     }
 }
