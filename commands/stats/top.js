e7f4ee6f9dedeae66f143e6c687fa7ff9c87bc2a
ItzFireable <fireableplayz@gmail.com>
2020-05-12 23:19:28 +0300

    haha bad code at 11 am go brrr

--- commands/stats/top.js
+++ commands/stats/top.js
@@ -1,5 +1,5 @@
 const { Command } = require("discord.js-commando");
-const { RichEmbed,MessageAttachment } = require("discord.js");
+const { RichEmbed,MessageAttachment,emojis } = require("discord.js");
 const request = require("request");
 const moment = require('moment');
 const Jimp = require('jimp');
@@ -87,17 +87,17 @@ module.exports = class topscores extends Command {
                         let date1 = new Date(body1.scores[0].time);
                         let since1 = moment(date1).fromNow()
 
-                        let Xrank = "<gradex:585556037319524352>";
-                        let SSrank = "<gradess:585555983921709087>";
-                        let Srank = "<grades:585555950623260673>";
-                        let Arank = "<gradea:585555734205562880>";
-                        let Brank = "<gradeb:585555793248780288>";
-                        let Crank = "<gradec:585555833564299284>";
-                        let Drank = "<graded:585555873951383554>";
-                        let Frank = "<gradef:585555916292751390>";
+                        let Xrank = emojis.get("585556037319524352");
+                        let SSrank = emojis.get("585555983921709087");
+                        let Srank = emojis.get("585555950623260673");
+                        let Arank = emojis.get("585555734205562880");
+                        let Brank = emojis.get("585555793248780288");
+                        let Crank = emojis.get("585555833564299284");
+                        let Drank = emojis.get("585555873951383554");
+                        let Frank = emojis.get("585555916292751390");
 
                         let stats1 = {
-                            "** 1. **": `${body1.scores[0].map.artist} - ${body1.scores[0].map.title} (${body1.scores[0].map.difficulty_name})`,
+                            "** **": `${body1.scores[0].map.artist} - ${body1.scores[0].map.title} (${body1.scores[0].map.difficulty_name})`,
                             "** Score▸ **": `${body1.scores[0].grade + "rank"} ${Math.round(body1.scores[0].accuracy * 100) / 100}% / ${Math.round(body1.scores[0].performance_rating * 100) / 100} QP`,
                             "** Info ▸ **": `${body1.scores[0].total_score} - x${body1.scores[0].max_combo} - [${body1.scores[0].count_marv}/${body1.scores[0].count_perf}/${body1.scores[0].count_great}/${body1.scores[0].count_good}/${body1.scores[0].count_okay}/${body1.scores[0].count_miss}]`,
                             "** Score set ▸ **": `${since1}`,
@@ -107,7 +107,7 @@ module.exports = class topscores extends Command {
                         let since2 = moment(date2).fromNow()
 
                         let stats2 = {
-                            "** 2. **": `${body1.scores[1].map.artist} - ${body1.scores[1].map.title} (${body1.scores[1].map.difficulty_name})`,
+                            "** **": `${body1.scores[1].map.artist} - ${body1.scores[1].map.title} (${body1.scores[1].map.difficulty_name})`,
                             "** Score ▸ **": `${body1.scores[1].grade + "rank"} ${Math.round(body1.scores[1].accuracy * 100) / 100}% / ${Math.round(body1.scores[1].performance_rating * 100) / 100} QP`,
                             "** Info ▸ **": `${body1.scores[1].total_score} - x${body1.scores[1].max_combo} - [${body1.scores[1].count_marv}/${body1.scores[1].count_perf}/${body1.scores[1].count_great}/${body1.scores[1].count_good}/${body1.scores[1].count_okay}/${body1.scores[1].count_miss}]`,
                             "** Score set ▸ **": `${since2}`,
@@ -117,7 +117,7 @@ module.exports = class topscores extends Command {
                         let since3 = moment(date3).fromNow()
 
                         let stats3 = {
-                            "** 3. **": `${body1.scores[2].map.artist} - ${body1.scores[2].map.title} (${body1.scores[2].map.difficulty_name})`,
+                            "** **": `${body1.scores[2].map.artist} - ${body1.scores[2].map.title} (${body1.scores[2].map.difficulty_name})`,
                             "** Score ▸ **": `${body1.scores[2].grade + "rank"} ${Math.round(body1.scores[2].accuracy * 100) / 100}% / ${Math.round(body1.scores[2].performance_rating * 100) / 100} QP`,
                             "** Info ▸ **": `${body1.scores[2].total_score} - x${body1.scores[2].max_combo} - [${body1.scores[2].count_marv}/${body1.scores[2].count_perf}/${body1.scores[2].count_great}/${body1.scores[2].count_good}/${body1.scores[2].count_okay}/${body1.scores[2].count_miss}]`,
                             "** Score set ▸ **": `${since3}`,
@@ -127,7 +127,7 @@ module.exports = class topscores extends Command {
                         let since4 = moment(date4).fromNow()
 
                         let stats4 = {
-                            "** 4. **": `${body1.scores[3].map.artist} - ${body1.scores[3].map.title} (${body1.scores[3].map.difficulty_name})`,
+                            "** **": `${body1.scores[3].map.artist} - ${body1.scores[3].map.title} (${body1.scores[3].map.difficulty_name})`,
                             "** Score ▸ **": `${body1.scores[3].grade + "rank"} ${Math.round(body1.scores[3].accuracy * 100) / 100}% / ${Math.round(body1.scores[3].performance_rating * 100) / 100} QP`,
                             "** Info ▸ **": `${body1.scores[3].total_score} - x${body1.scores[3].max_combo} - [${body1.scores[3].count_marv}/${body1.scores[3].count_perf}/${body1.scores[3].count_great}/${body1.scores[3].count_good}/${body1.scores[3].count_okay}/${body1.scores[3].count_miss}]`,
                             "** Score set ▸ **": `${since4}`,
@@ -137,7 +137,7 @@ module.exports = class topscores extends Command {
                         let since5 = moment(date5).fromNow()
 
                         let stats5 = {
-                            "** 5. **": `${body1.scores[4].map.artist} - ${body1.scores[4].map.title} (${body1.scores[4].map.difficulty_name})`,
+                            "** **": `${body1.scores[4].map.artist} - ${body1.scores[4].map.title} (${body1.scores[4].map.difficulty_name})`,
                             "** Score ▸ **": `${body1.scores[4].grade + "rank"} ${Math.round(body1.scores[4].accuracy * 100) / 100}% / ${Math.round(body1.scores[4].performance_rating * 100) / 100} QP`,
                             "** Info ▸ **": `${body1.scores[4].total_score} - x${body1.scores[4].max_combo} - [${body1.scores[4].count_marv}/${body1.scores[4].count_perf}/${body1.scores[4].count_great}/${body1.scores[4].count_good}/${body1.scores[4].count_okay}/${body1.scores[4].count_miss}]`,
                             "** Score set ▸ **": `${since5}`,
