5ec25705a35ad51c9f3ae4ab930db0b696991f75
ItzFireable <fireableplayz@gmail.com>
2020-05-12 15:29:52 +0300

    Removed link from Status

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ client.registry
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
     client.user.setActivity("Quaver", {
-        type: "STREAMING",
-        url: "https://www.quavergame.com"
+        type: "STREAMING"
     });
 });
 
