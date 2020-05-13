dbf4bc1225066b74b4561ad520d485451f140564
ItzFireable <fireableplayz@gmail.com>
2020-05-13 15:43:46 +0300

    Changed title order

--- commands/stats/top.js
+++ commands/stats/top.js
@@ -130,7 +130,7 @@ module.exports = class topscores extends Command {
                         }
 
                         //embed.setTitle("Global Rank: #" + keymodeObject.globalRank);
-                        embed.setTitle(`(${keymodeString}) ${emojiFlags.countryCode(body.user.info.country).emoji} ${name}'s top scores`);
+                        embed.setTitle(`${emojiFlags.countryCode(body.user.info.country).emoji} ${name}'s top scores (${keymodeString})`);
                         embed.setURL(`https://quavergame.com/profile/${body.user.info.id}?mode=${keysmode}`);
                         //embed.setDescription(`${emojiFlags.countryCode(body.user.info.country).emoji} Country Rank: #` + keymodeObject.countryRank);
                         embed.setTimestamp()
