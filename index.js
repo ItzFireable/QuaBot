cdc4e2c5c2df3e71c92082335edc24817d89d0a9
ItzFireable <fireableplayz@gmail.com>
2020-05-24 14:42:21 +0300

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager("./shard  .js",
+const manager = new ShardingManager("./shard.js",
     {
         token: process.env.BOT_TOKEN,
 	    totalShards: 'auto',
